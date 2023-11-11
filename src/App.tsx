048252a65583903c60e35d89385d5fb635c4e93c
omaar <omar16694@gmail.com>
2023-11-11 18:30:37 +0400

    feat(App.tsx): add react-router-dom for routing in App component feat(layout/layout.tsx): create layout component with Navbar and Outlet feat(layout/navbar/navbar.tsx): create Navbar component with navigation links feat(Home): create Home component for the home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Dropdown } from "./components";
+import { Home } from "./screens";
+import { Layout } from "./layout/layout";
 
 export const App = () => {
   return (
-    <div>
-      <section>
-        <h1 className="section-title">Dropdown</h1>
-        <Dropdown />
-      </section>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
+
+// <div>
+//   <section>
+//     <h1 className="section-title">Dropdown</h1>
+//     <Dropdown />
+//   </section>
+// </div>
