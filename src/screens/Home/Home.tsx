70e561efb8e4e0dea7295fb1d8a03317599697e5
omaar <omar16694@gmail.com>
2023-11-11 18:36:36 +0400

    style: update scrollbar styles in index.css style: add styles for Home screen content in Home.css feat: update Home component to include custom content and styles

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,5 +1,12 @@
 import "./Home.css";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <h1>React Custom Components</h1>
+        <p>Mainly learned from Otida</p>
+      </div>
+    </div>
+  );
 };
